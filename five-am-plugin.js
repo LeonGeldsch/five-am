9f43693c1395ac88d09e5ed4985d6f2f16c67330
LeonGeldsch <leon.geldschlaeger@gmail.com>
2021-02-10 15:06:17 +0100

    updated image hover effects and badge image collision

--- five-am-plugin.js
+++ five-am-plugin.js
@@ -20,6 +20,14 @@ var burgerLower = document.querySelector(".burger.lower");
 
 var hoverEffectImages = document.querySelectorAll(".object-wrapper, .lab_wrapper");
 
+var allImages = document.querySelectorAll(".case-image, .team-image");
+
+var bigTextPreFooter = document.querySelector(".big-text.pre-footer");
+
+var allCaseWrappers = document.querySelectorAll(".case_wrapper");
+
+var allButtonTextLights = document.querySelectorAll(".button-text.light");
+
 (function () {
 
     function loadScript(url, callback) {
@@ -55,7 +63,7 @@ var hoverEffectImages = document.querySelectorAll(".object-wrapper, .lab_wrapper
 
 
 window.addEventListener('scroll', function() {
-    if (parseInt(badgeWrapper.getBoundingClientRect().top) + parseInt(badgeWrapper.offsetHeight) >= parseInt(sectionGrey.getBoundingClientRect().top) && parseInt(badgeWrapper.getBoundingClientRect().top) + parseInt(badgeWrapper.offsetHeight) <= parseInt(sectionGrey.getBoundingClientRect().top) + parseInt(sectionGrey.offsetHeight)) {
+    if (parseInt(badgeWrapper.getBoundingClientRect().top) + parseInt(badgeWrapper.offsetHeight) * 0.5 >= parseInt(sectionGrey.getBoundingClientRect().top) && parseInt(badgeWrapper.getBoundingClientRect().top) + parseInt(badgeWrapper.offsetHeight) * 0.5 <= parseInt(sectionGrey.getBoundingClientRect().top) + parseInt(sectionGrey.offsetHeight)) {
         badge.style.filter = "invert()";
     } else {
         badge.style.filter = "none";
@@ -66,28 +74,67 @@ menuGradientLight.addEventListener("click", function() {
     menuWrapper.click();
 });
 
-console.log(hoverEffectImages);
+allCaseWrappers.forEach(caseWrapper => {
+    caseWrapper.style.overflow = "visible";
+});
+
+allButtonTextLights[1].style.color = "#333";
 
 
 $.getScript('https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js', function() {
     console.log("loaded gsap");
-    hoverEffectImages.forEach(hoverEffectImage => {
-        hoverEffectImage.addEventListener('mouseover', function () {
-            console.log("mouseover");
-            gsap.to(hoverEffectImage, {
-                scale: 1.15,
-                duration: 1,
-                ease: "power1"
-            }); 
+    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollTrigger.min.js', function() {
+        console.log("loaded scrolltrigger");
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.to(bigTextPreFooter, {
+            backgroundPosition: "50% 0%",
+            duration: 0
         });
-        hoverEffectImage.addEventListener('mouseleave', function () {
-            console.log("mouseleave");
-            gsap.to(hoverEffectImage, {
-                scale: 1,
-                duration: 1,
-                ease: "power1"
-            }); 
+
+        gsap.to(bigTextPreFooter, {
+            backgroundPosition: "50% 40%",
+            scrollTrigger: {
+                trigger: ".big-text.pre-footer",
+                start: "-=500",
+                end: "+=500",
+                scrub: true
+            }
+        });
+
+
+        hoverEffectImages.forEach(hoverEffectImage => {
+            hoverEffectImage.addEventListener('mouseover', function () {
+                gsap.to(hoverEffectImage, {
+                    scale: 1.05,
+                    duration: 0.4,
+                    ease: "power2"
+                }); 
+            });
+            hoverEffectImage.addEventListener('mouseleave', function () {
+                gsap.to(hoverEffectImage, {
+                    scale: 1,
+                    duration: 0.4,
+                    ease: "power2"
+                }); 
+            });
+        });
+    
+        allImages.forEach(image => {
+            image.addEventListener('mouseover', function () {
+                gsap.to(image, {
+                    borderRadius: 20,
+                    duration: 0.2,
+                    scale: 1.02
+                });
+            });
+            image.addEventListener('mouseleave', function () {
+                gsap.to(image, {
+                    borderRadius: 0,
+                    duration: 0.2,
+                    scale: 1
+                });
+            });
         });
     });
 });
-
